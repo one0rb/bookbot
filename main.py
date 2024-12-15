ffee2c0236b08d37d4f867a8f9135d1ff63055bf
Andrew Ball <one0rb.git@gmail.com>
2024-12-15 15:36:12 +0800

    Update main.py to print report
    Added functions to sort dictionary into descending numerical order
    and print a stylised report of the number of characters.

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_dict = get_char_dict(text)
-    print(f"This document has {num_words} words.")
-    print(char_dict)
+    sorted_char_list = char_dict_to_sorted_list(char_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words were found in this document.")
+    for item in sorted_char_list:
+        if item["char"].isalpha():
+            print(f"The {item['char']} character was found {item['num']} times.")
+    print("--- End of report ---")
     
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def char_dict_to_sorted_list(dict):
+    sorted_list = []
+    for key in dict:
+        new_dict = {"char": key, "num": dict[key]}
+        sorted_list.append(new_dict)
+        sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 
 def get_char_dict(text):
     char_dict = {}
