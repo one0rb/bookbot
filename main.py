c84b0c1a5038c908f178e0753dbad57965b07a65
Andrew Ball <one0rb.git@gmail.com>
2024-12-15 15:29:10 +0800

    Update main.py to count characters
    Returns character count as a dictionary

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_dict = get_char_dict(text)
     print(f"This document has {num_words} words.")
+    print(char_dict)
+    
+
+def get_char_dict(text):
+    char_dict = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in char_dict:
+            char_dict[lowered] += 1
+        else:
+            char_dict[lowered] = 1
+    return char_dict
 
 
 def get_num_words(text):
